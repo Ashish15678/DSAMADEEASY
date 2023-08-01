0155bee8a37259f625b3e07a36e8d508bf19f0e4
Ashish Kumar <15678ashish@gmail.com>
2023-08-02 02:50:13 +0530

    changes

--- Stacks/Readme.md
+++ Stacks/Readme.md
@@ -13,6 +13,38 @@
 2. Using dynamic arrays(growable arrays)
 3. Using Linked List
 
-
 ### Implementation using Arrays
 
+### Implementation using Linked list
+
+1. First create a new data type Linked List having data and pointer to the next Node.
+
+```c
+struct listNode
+{
+    int data;
+    struct listNode *next;
+};
+
+```
+
+2. Then create a new data type Stack which will have only one pointer of Linked list type which will always point to the top of the Stack.
+
+```c
+struct Stack{
+    struct listNode *top;
+};
+```
+
+3. Create createStack function in order to initialize Stack
+
+```c
+struct Stack *createStack()
+{
+    struct Stack *stack = malloc(sizeof(struct Stack));
+    stack->top = -1;
+    return stack;
+}
+```
+
+4. Creating All other necessary Functions used in stack
--- Stacks/uisngLinkedList.c
+++ Stacks/uisngLinkedList.c
@@ -8,8 +8,55 @@ struct listNode
     struct listNode *next;
 };
 
+struct Stack
+{
+    struct listNode *top;
+};
+
+struct Stack *createStack()
+{
+    struct Stack *stack = malloc(sizeof(struct Stack));
+    stack->top = -1;
+    return stack;
+}
+
+void push(struct Stack *stack, int data)
+{
+    struct listNode *temp = malloc(sizeof(struct listNode));
+    // checking for memory errors
+    if (!temp)
+    {
+        printf("Memory error ... node not created");
+        return;
+    }
+
+    if (stack->top == -1)
+    {
+        stack->top = temp;
+        temp->next = NULL;
+    }
+    else
+    {
+        temp->next = stack->top;
+        stack->top = temp;
+    }
+}
+
+int size(struct Stack *stack)
+{
+    int counter = 0;
+    while (stack->top->next != NULL)
+    {
+        counter++;
+    }
+    return counter;
+}
+
 int main(int argc, char const *argv[])
 {
-
+    struct Stack *stack = createStack();
+    push(stack, 1);
+    push(stack, 2);
+    printf("%d", size(stack));
     return 0;
 }
